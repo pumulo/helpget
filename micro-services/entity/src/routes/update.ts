6d63d8abfc197288a5e77d3cf30c37646ace6779
Pumulo Sikaneta <pumulo@gmail.com>
2023-06-24 15:46:58 -0400

    Entity microservice add new function

--- micro-services/entity/src/routes/update.ts
+++ micro-services/entity/src/routes/update.ts
@@ -16,7 +16,7 @@ router.put(
     ],
     // validateRequest,
     async (req: Request, res: Response) => {
-        const { id, type, description, values, status } = req.body;
+        let { id, type, description, values, status } = req.body;
         
         // if we find an existing entity, update it
         if (id && id != 'New') {
@@ -30,6 +30,13 @@ router.put(
             }
         }
         //otherwise create a new one
+        if (!status) {
+            status = "New_UnknownStatus";
+        }
+
+        if (!type) {
+            type = "Unknown";
+        }
         const entity = Entity.build({
             type,
             description,
@@ -43,5 +50,45 @@ router.put(
     }
 );
 
+router.put(
+    `${baseUrl}/:type/update`,
+    [
+        body('description').trim().isLength(
+                {max: 2000}
+        ).withMessage(
+            'The Entity description cannot be longer than 2000 characters'),
+    ],
+    // validateRequest,
+    async (req: Request, res: Response) => {
+        const type = req.params.type;
+        let { id, description, values, status } = req.body;
+        
+        // if we find an existing entity, update it
+        if (id && id != 'New') {
+            const existingEntity = await Entity.findById(id);
+
+            console.log(JSON.stringify(existingEntity));
+            if (existingEntity) {
+                existingEntity.values = values;
+                existingEntity.save();
+                res.status(202).send(existingEntity);
+            }
+        }
+        //otherwise create a new one
+        if (!status) {
+            status = "New_UnknownStatus";
+        }
+        const entity = Entity.build({
+            type,
+            description,
+            values,
+            status
+        });
+        entity.save();
+
+        res.status(201).send(entity);
+        
+    }
+);
 
 export { router as updateRouter };
