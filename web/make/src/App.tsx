8380c53bd574e0c8e83854f0d54ddb24662b37ad
Pumulo Sikaneta <pumulo@gmail.com>
2023-07-09 08:12:25 -0400

    add images in src folder and add to home page

--- web/make/src/App.tsx
+++ web/make/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import splash from './assets/images/get_IT_splash.jpg';
 
 const App = () => {
     return (
-        <div>
-            get it MADE!!!
+        <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+            <img src={splash} alt="Get-IT Solutions"></img>
         </div>
     );
 };
